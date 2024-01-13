506855b5c9561117001a2dc412d95285a0c92dc4
Roman Surkov <s.rb@list.ru>
2024-01-13 16:42:14 +0200

    Added ansible certificate

--- src/ExperienceData.js
+++ src/ExperienceData.js
@@ -156,6 +156,11 @@ export const CoreCompetencies = [
 export const Certifications = [
     {
         company: "Stepik",
+        date: "01.2024",
+        name: "Ansible for beginners + hands-on experience",
+    },
+    {
+        company: "Stepik",
         date: "12.2023",
         name: "Kubernetes for users",
     },{
