f700ce5284f2bcbcff33df2dfba10c09115f740d
Roman Surkov <s.rb@list.ru>
2024-11-04 11:05:45 +0200

    Заполнение описание в странице About по данным с гитхаба

--- src/Summary.js
+++ src/Summary.js
@@ -1,24 +1,7 @@
 import React from "react";
 
-export const Summary = <>
-    <p>Experienced and results-oriented Java Developer with a proven track record of developing high-quality software
-        solutions. Worked with major modern frameworks and technologies. Over 5 years of experience in developing and
-        implementing complex projects across various industries, including the Fintech sector.
-    </p>
-    <p>Throughout my career, I have demonstrated a passion for software development and a commitment to
-        continuous learning and growth. I have successfully collaborated with cross-functional teams to
-        streamline the software development process and enhance code quality. Skilled in team leadership,
-        I have effectively managed and mentored junior developers, fostering a collaborative and
-        productive work environment.
-    </p>
-    <p>After spending 10 years working in the designing of engineering systems, leading design teams,
-        and managing project implementation following my graduation in 2009, my strong desire to pursue
-        a career in software development led me to become a Java developer.
-    </p>
-    <p>I am driven by the challenge of solving complex problems and thrive in dynamic environments
-        where I can apply my strong problem-solving skills and attention to detail. With a focus on
-        delivering exceptional results, I am dedicated to surpassing customer expectations and
-        driving business success.
-    </p>
-    <p>I am seeking a new opportunity where I can leverage my experience to contribute to the success of a promising company.</p>
-</>;
+export const Summary = ({description}) => {
+    return (<React.Fragment>
+        {description && description.length > 0 && description?.map((o, idx) => <p key={`pac${idx}`}>{o}</p>)}
+    </React.Fragment>)
+}
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import {Summary} from "../../Summary";
+import {experienceData as defaultExpData} from "../../data/experienceData";
 
 function AboutCard() {
-    return (
+    let experienceData  = defaultExpData; // Данные по умолчанию
+    // // Проверяем, существует ли файл с загруженными данными
+    try {
+        experienceData = require("../../data/fetchedWorkExperience.json");
+    } catch (error) {
+        console.log("Using default local data");
+    }
 
+    return (
         <Card className="quote-card-view">
             <div className="blockquote mb-0 p-3" style={{textAlign: "justify"}}>
                 <div style={{ textAlign: "center"}}><p>Hello everybody. My name is <span className="blue">Roman Surkov </span></p></div>
-                <div style={{fontSize: "1.05rem"}}>{Summary}</div>
+                <div style={{fontSize: "1.05rem"}}><Summary description={experienceData?.description}/></div>
             </div>
         </Card>
     );
